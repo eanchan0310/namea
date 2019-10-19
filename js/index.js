b1a2f0a34e6d72903b8b6486a7f9931b1f1f3a21
eanchan0310 <56179170+eanchan0310@users.noreply.github.com>
2019-10-19 09:16:27 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    scene: {
+        create: create,
+    }
+};
 
 
-console.log('index.js file opened')
 
-document.getElementById('hello').addEventListener('click', function() {
-    console.log('hello console');
-})
+
+
+
+var game = new Phaser.Game(config);
+
+function create() 
+{
+    //  Default text with no style settings
+    this.add.text(100, 100, 'Phaser');
+
+    //  Pass in a basic style object with the constructor
+    this.add.text(100, 200, 'Phaser', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
+
+    //  Or chain calls like this:
+    this.add.text(100, 400, 'Phaser').setFontFamily('Arial').setFontSize(64).setColor('#ffff00');
+}
