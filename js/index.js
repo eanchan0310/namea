37673a628c8414c8f3014260eedf678c06b6df9a
eanchan0310 <56179170+eanchan0310@users.noreply.github.com>
2019-10-19 09:57:20 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,23 @@ var config = {
 
 
 
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
 
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'#f44336');
 
 
 var game = new Phaser.Game(config);
