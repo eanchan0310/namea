6c1c408f860039991c135e4d252f5d62b6baacbf
eanchan0310 <56179170+eanchan0310@users.noreply.github.com>
2020-04-25 12:33:35 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'phaser-example',
-    scene: {
-        create: create,
-    }
-};
-
-
-
 var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
 
@@ -27,18 +15,3 @@ function showPanel(panelIndex,colorCode) {
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
 showPanel(0,'#f44336');
-
-
-var game = new Phaser.Game(config);
-
-function create() 
-{
-    //  Default text with no style settings
-    this.add.text(100, 100, 'Phaser');
-
-    //  Pass in a basic style object with the constructor
-    this.add.text(100, 200, 'Phaser', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
-
-    //  Or chain calls like this:
-    this.add.text(100, 400, 'Phaser').setFontFamily('Arial').setFontSize(64).setColor('#ffff00');
-}
